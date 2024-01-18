4285cf2eb4d4fedd5dea1d15e660a4978e584b2a
Thiago <tlucenalive@gmail.com>
2024-01-18 11:59:13 -0300

    2 - Loops e tentativas

---
+++ Loops_e_tentativas/desafio.js
@@ -0,0 +1,33 @@
+console.log("Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.")
+let contador = 1;
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+   }
+
+console.log("Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.")
+let numero = 10;
+while (numero > 0) {{
+    console.log(numero);
+    numero--;
+    }    
+}
+
+
+console.log("Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador")
+let regressiva = prompt("Digite um número");
+while (regressiva >= 0) {
+    console.log(`Contagem regressiva ${regressiva}`);
+    regressiva--;
+    
+}
+console.log("Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.")
+let progressiva = prompt("Digite um número");
+let number = 0;
+while(number <= progressiva){
+    console.log(progressiva)
+    progressiva++
+}
+
+
+
